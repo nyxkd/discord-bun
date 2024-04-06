d5876af9e1261cf793efa3d2eb2a1a5c8188e437
Dragomir Mihai <mihai@nyxkd.ro>
2024-04-06 22:50:48 +0300

    updated logging

--- events/ready.ts
+++ events/ready.ts
@@ -5,7 +5,7 @@ import type { Event } from '../globals';
 const readyEvent: Event<Events.ClientReady> = {
     once: true,
     execute: async (client) => {
-        client.logger.log('silly', `Logged in as ${client.user?.tag}!`);
+        client.logger.log('event', `Logged in as ${client.user?.tag}!`);
     }
 }
 
