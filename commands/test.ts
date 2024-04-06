0627b914ac66cb9ca7e738b6c17f62363c7ddae6
Dragomir Mihai <mihai@nyxkd.ro>
2024-04-06 22:27:22 +0300

    added permissions

--- commands/test.ts
+++ commands/test.ts
@@ -1,19 +1,19 @@
-import { SlashCommandBuilder, EmbedBuilder, Colors } from "discord.js";
+import { SlashCommandBuilder, EmbedBuilder, Colors, PermissionFlagsBits } from "discord.js";
 
 import type { ChatInputCommandInteraction } from "discord.js";
 
 const command: Command<ChatInputCommandInteraction> = {
     data: new SlashCommandBuilder()
         .setName('test')
-        .setDescription('Test command'),
+        .setDescription('Test command')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     execute: async (interaction) => {
         const embed = new EmbedBuilder()
             .setTitle('Test')
             .setColor(Colors.Green);
 
         await interaction.reply({
-            embeds: [embed],
-            ephemeral: true
+            embeds: [embed]
         });
     }
 };
