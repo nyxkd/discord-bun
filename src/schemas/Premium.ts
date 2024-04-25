500da548a8299148ede26a03eb943e2801ce4c78
Dragomir Mihai <mihai@nyxkd.ro>
2024-04-25 13:15:40 +0300

    got rid of the id key

--- src/schemas/Premium.ts
+++ src/schemas/Premium.ts
@@ -1,14 +1,10 @@
 import { Model, DataTypes, type InferAttributes, type InferCreationAttributes } from '@sequelize/core';
 
-import { Attribute, AutoIncrement, NotNull, PrimaryKey } from '@sequelize/core/decorators-legacy';
+import { Attribute, NotNull, PrimaryKey } from '@sequelize/core/decorators-legacy';
 
 export class Premium extends Model<InferAttributes<Premium>, InferCreationAttributes<Premium>> {
-    @Attribute(DataTypes.INTEGER)
-    @PrimaryKey
-    @AutoIncrement
-    declare id: number;
-
     @Attribute(DataTypes.STRING)
+    @PrimaryKey
     @NotNull
     declare code: string;
 }
